efc9ccc98546ef2b7b72d4f5bbed3fdbbb45f394
Moody Rahman <moody.byte@gmail.com>
2020-12-21 13:56:29 -0500

    express dummy data

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get("/", (req, res) => {
-	res.send("Hello world!");
+app.get("/api", (req, res) => {
+	res.send({"data":"true"});
 });
 
 // start the Express server
