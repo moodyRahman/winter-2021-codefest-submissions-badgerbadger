b9e52bcec537b15fb779d2f4eaacc238a7b3870e
Moody Rahman <moody.byte@gmail.com>
2020-12-29 15:21:44 -0500

    api route returns dummy data

--- api/index.ts
+++ api/index.ts
@@ -7,6 +7,15 @@ app.get('/', (req, res) =>
 	res.send('Express + TypeScript Server')
 );
 
+app.get('/rawdata', (req, res) => {
+	const rawdata = {
+		pluralism_and_diversity:["mood101, mood102", "mood103"],
+		writing_intensive:["alex101", "mood103", "tanmoy203"],
+		stem:["alex101", "monique202"],
+		major:["monique202"]
+	}
+	res.send(rawdata)
+});
 
 
 app.listen(PORT, () => {
