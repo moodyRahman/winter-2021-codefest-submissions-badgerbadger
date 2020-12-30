6fbc434ff70b7ebc0670b8082b9e267b1c7139d4
Moody Rahman <moody.byte@gmail.com>
2020-12-30 06:52:10 -0500

    raw data type

--- api/index.ts
+++ api/index.ts
@@ -6,17 +6,36 @@ const PORT = 8080;
 app.use(cors())
 
 
-app.get('/', (req, res) => 
+app.get('/', (req, res) =>
 	res.send('Express + TypeScript Server')
 );
 
+interface ReqData {
+	requirement: string;
+	fulfilling_classes: string[];
+}
+
+
 app.get('/rawdata', (req, res) => {
-	const rawdata = {
-		pluralism_and_diversity:["mood101, mood102", "mood103"],
-		writing_intensive:["alex101", "mood103", "tanmoy203"],
-		stem:["alex101", "monique202"],
-		major:["monique202"]
-	}
+
+	const rawdata: ReqData[] = [
+		{
+			requirement: "pluralism_and_diversity",
+			fulfilling_classes: ["mood101, mood102", "mood103"]
+		},
+		{
+			requirement: "writing_intensive",
+			fulfilling_classes: ["alex101", "mood103", "tanmoy203"]
+		},
+		{
+			requirement: "stem",
+			fulfilling_classes: ["alex101", "monique202"]
+		},
+		{
+			requirement:"major",
+			fulfilling_classes: ["monique202"]
+		}
+	]
 	res.send(rawdata)
 });
 
