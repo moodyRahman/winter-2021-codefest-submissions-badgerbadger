cb8c55fd38b928213ed70e67e2335318d9faf912
Moody Rahman <moody.byte@gmail.com>
2021-01-02 18:03:02 -0500

    login route accesses database

--- apps/backend/src/routes/auth/login.ts
+++ apps/backend/src/routes/auth/login.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 
+import { dbconnect } from "../../database/database";
+
 const route = Router();
+dbconnect();
 
 route.post("/login", (req, res) => {});
 
 export default route;
+
