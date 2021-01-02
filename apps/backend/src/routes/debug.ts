42f051e9c27677b24607e5d881360f2de60942f3
Moody Rahman <moody.byte@gmail.com>
2021-01-02 18:43:27 -0500

    create new user based on post-ed data from /debug

--- apps/backend/src/routes/debug.ts
+++ apps/backend/src/routes/debug.ts
@@ -1,14 +1,22 @@
 import { Router } from "express";
-
+import {User} from "../database/models/models"
 const route = Router();
 
 route.post("/debug", (req, res) => {	
 	console.log(req.body);
+
+	const moody = new User(req.body);
+
+	moody.save( (err, moody) => {
+		if (err){
+			return console.log(err);
+		}
+	});
+	
 	res.send(req.body);
 });
 
 route.get("/debug", (_req, res) => {
-
 	res.send("aye this route is alive and well");
 });
 
