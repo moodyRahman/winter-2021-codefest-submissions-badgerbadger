135baef5d5261104f1f8366f6a6578c677d22080
Moody Rahman <moody.byte@gmail.com>
2021-01-02 18:02:52 -0500

    database connection boilerplate code

---
+++ apps/backend/src/database/database.ts
@@ -0,0 +1,29 @@
+require('dotenv').config()
+import Mongoose from "mongoose";
+
+let database: Mongoose.Connection;
+
+export const dbconnect = () => {
+	const uri: string = `${process.env.MONGO_URI}`;
+
+	if (database){
+		return true;
+	}
+
+	Mongoose.connect(uri, {
+		useNewUrlParser: true,
+		useFindAndModify: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	});
+
+	database = Mongoose.connection;
+
+	database.once("open", async () => {
+		console.log("Connected to database");
+	});
+
+	database.on("error", () => {
+		console.log("Error connecting to database");
+	});
+}
