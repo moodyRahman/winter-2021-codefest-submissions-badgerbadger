fb995631f7d9356fdb802081251021067c36648a
Moody Rahman <moody.byte@gmail.com>
2021-01-02 20:31:17 -0500

    removed salt field [bcrypt]

--- apps/backend/src/database/schemas/user.schema.ts
+++ apps/backend/src/database/schemas/user.schema.ts
@@ -3,12 +3,10 @@ import { Schema, Document } from "mongoose";
 export interface IUser extends Document {
 	username: string;
 	hashed_password: string;
-	salt:string;
 }
 
 
 export const UserSchema: Schema = new Schema({
 	username: String,
-	hashed_password: String,
-	salt:String
+	hashed_password: String
 });
