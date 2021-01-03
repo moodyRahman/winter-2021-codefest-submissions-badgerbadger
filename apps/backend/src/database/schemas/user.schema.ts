681876e345b7a9ad78fcbd17f812e57103598e3c
Moody Rahman <moody.byte@gmail.com>
2021-01-02 19:32:16 -0500

    salt fields in user model

--- apps/backend/src/database/schemas/user.schema.ts
+++ apps/backend/src/database/schemas/user.schema.ts
@@ -3,10 +3,12 @@ import { Schema, Document } from "mongoose";
 export interface IUser extends Document {
 	username: string;
 	hashed_password: string;
+	salt:string;
 }
 
 
 export const UserSchema: Schema = new Schema({
 	username: String,
-	hashed_password: String
+	hashed_password: String,
+	salt:String
 });
