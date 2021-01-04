1f2047829a5a35849f6bf8f6163721dce27e5326
isabiiil <isabelabonitalla@gmail.com>
2021-01-04 11:31:35 -0500

    cleaned up after restructure

---
+++ apps/frontend/src/components/SearchResult.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./SearchResult.scss";
+
+export default function SearchResult({}) {
+  return (
+    <div className="search-result">
+      <div className="fulfilled">
+        <div className="GrpA"></div>
+        <div className="GrpB"></div>
+      </div>
+    </div>
+  )
+}
