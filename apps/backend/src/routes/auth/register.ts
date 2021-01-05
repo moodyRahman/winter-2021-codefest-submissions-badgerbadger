85ad2ef34ea932aaa41b15a5575a57fd6c74b6d3
Moody Rahman <moody.byte@gmail.com>
2021-01-04 21:34:04 -0500

    skeleton for register

--- apps/backend/src/routes/auth/register.ts
+++ apps/backend/src/routes/auth/register.ts
@@ -1,7 +1,18 @@
 import { Router } from "express";
-
+import { User, IUser } from "../../database/models"
+import { HTTP_CODE } from "../../../../shared/enums/httpcodes";
 const route = Router();
 
-route.post("/register", (req, res) => {});
+route.post("/register", (req, res) => {
+	User.find({ username: req.body.username }, (err, docs) => {
+		
+		if (docs.length != 0) {
+			return res.send({ status: HTTP_CODE.FORBIDDEN, message: "user already exists" })
+		}
+
+		return res.send({status:HTTP_CODE.OK, message:"temp message"});
+
+	})
+});
 
 export default route;
