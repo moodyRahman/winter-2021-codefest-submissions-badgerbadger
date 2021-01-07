f1b2138281d1ae92a9351ebe5695a89c0ecd6510
Alex <alexy4744@gmail.com>
2021-01-07 17:22:01 -0500

    hash with bcrypt in user pre save hook

--- apps/backend/src/database/schemas/user.schema.ts
+++ apps/backend/src/database/schemas/user.schema.ts
@@ -1,22 +1,34 @@
+import bcrypt from "bcrypt";
+
 import { Schema, Document } from "mongoose";
-import { model } from "mongoose"
+import { model } from "mongoose";
 
 export interface IUser extends Document {
-	username: string;
-	hashed_password: string;
+  password: string;
+  username: string;
 }
 
-
 export const UserSchema: Schema = new Schema({
-	username: {
-		type: String,
-		required: true
-	},
-	hashed_password: {
-		type: String,
-		required: true
-	}
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
 });
 
+UserSchema.pre<IUser>("save", function (next) {
+  if (!this.isModified("password")) return next();
 
-export const User = model<IUser>('User', UserSchema);
+  bcrypt.hash(this.password, 12, (err, hash) => {
+    if (err) return next(err);
+
+    this.password = hash;
+
+    next();
+  });
+});
+
+export const User = model<IUser>("User", UserSchema);
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -3,13 +3,13 @@ require("module-alias/register");
 
 import cors from "cors";
 import express from "express";
-import bodyParser from "body-parser"
+import bodyParser from "body-parser";
 import Mongoose from "mongoose";
 
 import auth from "./routes/auth";
 import filter from "./routes/filter";
 import rawData from "./routes/raw-data";
-import debug from "./routes/debug"
+import debug from "./routes/debug";
 
 const app = express();
 
@@ -24,27 +24,28 @@ app.use(filter);
 app.use(rawData);
 app.use(debug);
 
-Mongoose.connection.on("error", err => {
-	console.log(err);
+Mongoose.connection.on("error", (err) => {
+  console.log(err);
 });
 
 Mongoose.connection.once("open", () => {
-	console.log(`🐵[database][${new Date().toLocaleTimeString()}]: connected to remote`);
+  console.log(
+    `🐵[database][${new Date().toLocaleTimeString()}]: connected to remote`
+  );
 });
 
 (async () => {
-	await Mongoose.connect(`${process.env.MONGO_URI}`, {
-			useNewUrlParser: true,
-			useFindAndModify: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-	})
+  await Mongoose.connect(`${process.env.MONGO_URI}`, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
 
-	app.listen(process.env.PORT, () => {
-		console.log(
-			`⚡[server][${new Date().toLocaleTimeString()}]:`,
-			`running on https://localhost:${process.env.PORT}`
-		);
-	});
-
-}) ();
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `⚡[server][${new Date().toLocaleTimeString()}]:`,
+      `running on https://localhost:${process.env.PORT}`
+    );
+  });
+})();
