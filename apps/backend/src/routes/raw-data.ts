093005546d55c80047b161f9afbc63b8540e5108
Alex <alexy4744@gmail.com>
2021-01-14 20:28:13 -0800

    remove comments from raw data route

--- apps/backend/src/routes/raw-data.ts
+++ apps/backend/src/routes/raw-data.ts
@@ -1,55 +1,8 @@
 import { Router } from "express";
 
-import { Class } from "@shared/interfaces/class";
-
 const route = Router();
 
-// given an array of ReqData
-// which class fulfills multiple req's?
-// returns a JSON containing {classname:occurences}
-// const getMultiReqClasses = (classes: Class[]) => {
-//   let out: any = {};
-//   for (let x of classes) {
-//     for (let y of x.fulfilling_classes) {
-//       // if the class has not been seen before
-//       if (!out.hasOwnProperty(y)) {
-//         // set it to 0
-//         out[y] = 0;
-//         continue;
-//       }
-//       // increment it otherwise
-//       out[y]++;
-//     }
-//   }
-
-//   let formatted_out = [];
-//   for (var i in out) {
-//     formatted_out.push([i, out[i]]);
-//   }
-//   return formatted_out;
-// };
-
 route.get("/raw-data", (_req, res) => {
-  // const rawdata: Class[] = [
-  //   {
-  //     fulfills: ["pluralism_and_diversity"],
-  //     prereqs: ["mood101", "mood102", "mood103"],
-  //   },
-  //   {
-  //     fulfills: ["writing_intensive"],
-  //     prereqs: ["alex101", "mood103", "tanmoy203"],
-  //   },
-  //   {
-  //     fulfills: ["stem"],
-  //     prereqs: ["alex101", "monique202"],
-  //   },
-  //   {
-  //     fulfills: ["major"],
-  //     prereqs: ["monique202"],
-  //   },
-  // ];
-  // console.log(getMultiReqClasses(rawdata));
-  // res.send(rawdata);
 });
 
 export default route;
