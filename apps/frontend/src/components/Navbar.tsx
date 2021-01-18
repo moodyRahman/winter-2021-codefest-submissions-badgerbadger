58ca3df74d2612283262ac55d244bd55d1842ed8
Moody Rahman <moody.byte@gmail.com>
2021-01-18 18:27:17 -0500

    navbars displays what's in the Context for debugging

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import "./Navbar.scss";
+import {TokenContext} from "../context/TokenContext"
 
 // const logo =  require("./../logo.png")
 
 export default function Navbar() {
+  const {token, setToken} = useContext(TokenContext);
   return (
     <div className="navbar">
       <p className="logo">insert logo here</p>
@@ -12,6 +14,7 @@ export default function Navbar() {
         <p>Current</p>
         <p>Complete</p>
         <p>Log Out</p>
+        <p>{token}</p>
       </div>
     </div>
   )
